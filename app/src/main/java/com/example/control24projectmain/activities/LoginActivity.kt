7ef06f70e2802ec871ee049a6f04a97081027de0
dmitry <dima.valbu.01@gmail.com>
2023-03-06 18:57:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/control24projectmain/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.control24projectmain.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.control24projectmain.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
