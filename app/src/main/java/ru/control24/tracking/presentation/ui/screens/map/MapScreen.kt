9601d098242fd3b5217dae1dbc193f7613b212a0
Avvami <dima.valbu.01@gmail.com>
2024-05-13 21:41:48 +0700

    Mapbox

--- app/src/main/java/ru/control24/tracking/presentation/ui/screens/map/MapScreen.kt
+++ app/src/main/java/ru/control24/tracking/presentation/ui/screens/map/MapScreen.kt
@@ -1,16 +1,15 @@
 package ru.control24.tracking.presentation.ui.screens.map
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.mapbox.geojson.Point
+import ru.control24.tracking.core.components.MapBoxMap
 
 @Composable
 fun MapScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Landing soon", style = MaterialTheme.typography.titleLarge)
-    }
+    MapBoxMap(
+        modifier = Modifier.fillMaxSize(),
+        point = Point.fromLngLat(-0.6333, 35.6971)
+    )
 }
